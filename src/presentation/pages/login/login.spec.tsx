0ee0291d12f2e00c6c45cff212c02111d8964ffa
Rafael Del Grossi <rafa.comp@gmail.com>
2022-06-19 20:14:12 -0300

    test: ensure login presents valid state if password is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -80,4 +80,14 @@ describe('Login Component', () => {
     expect(emailStatus.title).toBe('Tudo certo')
     expect(emailStatus.textContent).toBe('🟢')
   })
+
+  test('Should show valid password state if Validation succeeds', () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.errorMessage = null
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
+    const passwordStatus = sut.getByTestId('password-status')
+    expect(passwordStatus.title).toBe('Tudo certo')
+    expect(passwordStatus.textContent).toBe('🟢')
+  })
 })
