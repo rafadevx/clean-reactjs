08361e007fba7aabd40156ef3c0b31e939eb9ad3
Rafael Del Grossi <rafa.comp@gmail.com>
2022-07-27 09:49:07 -0300

    test: ensure ValidationBuilder has a required helper

---
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -0,0 +1,40 @@
+import { FieldValidationSpy } from '@/validation/test/mock-field-validation'
+import { ValidationComposite } from './validation-composite'
+import faker from 'faker'
+
+type SutTypes = {
+  sut: ValidationComposite
+  fieldValidationSpies: FieldValidationSpy[]
+}
+
+const makeSut = (fieldName: string): SutTypes => {
+  const fieldValidationSpies = [
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName)
+  ]
+
+  const sut = new ValidationComposite(fieldValidationSpies)
+  return {
+    sut,
+    fieldValidationSpies
+  }
+}
+
+describe('ValidationComposite', () => {
+  test('Should return error if any validation fails', () => {
+    const fieldName = faker.database.column()
+    const { sut, fieldValidationSpies } = makeSut(fieldName)
+    const errorMessage = faker.random.words()
+    fieldValidationSpies[0].error = new Error(errorMessage)
+    fieldValidationSpies[1].error = new Error(faker.random.words())
+    const error = sut.validate(fieldName, faker.random.word())
+    expect(error).toBe(errorMessage)
+  })
+
+  test('Should return falsy if all validation are successfull', () => {
+    const fieldName = faker.database.column()
+    const { sut } = makeSut(fieldName)
+    const error = sut.validate(fieldName, faker.random.word())
+    expect(error).toBeFalsy()
+  })
+})
